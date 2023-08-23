439c49c49b6a430a7d2fabf70c16ee102ab165ea
Rahul4dev <rahulsinghsignin@gmail.com>
2023-08-23 23:04:05 +0530

    ContextAPI_insteadOF_Redux

--- src/context/products-context.js
+++ src/context/products-context.js
@@ -29,13 +29,30 @@ const initialProductsList = [
 
 export const ProductsContext = createContext({
   products: [],
+  toggleFav: (id) => {},
 });
 
 const ProductsProvider = (props) => {
   const [productsList, setProductsList] = useState(initialProductsList);
 
+  const toggleFavorite = (productId) => {
+    setProductsList((currentProdList) => {
+      const prodIndex = currentProdList.findIndex((p) => p.id === productId);
+      const newFavStatus = !currentProdList[prodIndex].isFavorite;
+      const updatedProducts = [...currentProdList];
+      updatedProducts[prodIndex] = {
+        ...currentProdList[prodIndex],
+        isFavorite: newFavStatus,
+      };
+
+      return updatedProducts;
+    });
+  };
+
   return (
-    <ProductsContext.Provider value={{ products: productsList }}>
+    <ProductsContext.Provider
+      value={{ products: productsList, toggleFav: toggleFavorite }}
+    >
       {props.children}
     </ProductsContext.Provider>
   );
